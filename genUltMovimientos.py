3f731cbb8aa6b1abfc656eff1834cfb269058a8f
MarianoImende <98611190+MarianoImende@users.noreply.github.com>
2023-10-18 10:53:10 -0300

    Update genUltMovimientos.py

--- genUltMovimientos.py
+++ genUltMovimientos.py
@@ -1,6 +1,9 @@
 import random
 from datetime import datetime, timedelta
 
+import random
+from datetime import datetime, timedelta
+
 def generarFechas(fecha_desde, fecha_hasta):
     # Convertir las fechas de cadena a objetos datetime
     fecha_desde_dt = datetime.strptime(fecha_desde, "%Y%m%d")
@@ -18,12 +21,21 @@ def generarFechas(fecha_desde, fecha_hasta):
         # Formatear la fecha como "yyyymmdd"
         fecha_formateada = fecha_generada.strftime("%Y%m%d")
         
+        # Elegir una descripción aleatoria
+        descripcion = random.choice(["Compra", "Plazo Fijo ingreso", "Ingreso en efectivo", "Transferencia Credito", "Depósito en efectivo", "Retiro de cajero automático", "Transferencia a otra cuenta", "Pago de impuestos y servicios"])
+        
+        # Determinar si el monto debe ser negativo
+        if descripcion in ["Retiro de cajero automático", "Transferencia a otra cuenta", "Compra", "Pago de impuestos y servicios"]:
+            monto = round(random.uniform(-10000, -1), 2)  # Monto negativo
+        else:
+            monto = round(random.uniform(1, 10000), 2)  # Monto positivo
+        
         # Crear un diccionario para el movimiento
         movimiento = {
             "fecha": fecha_formateada,
-            "tipo": random.choice(["Depósito", "Retiro", "Transferencia", "Pago"]),
-            "monto": round(random.uniform(-1000, 1000), 2),
-            "descripcion": random.choice(["Depósito en efectivo", "Retiro de cajero automático", "Transferencia a otra cuenta", "Pago de impuestos y servicios"])
+            #"tipo": random.choice(["Depósito", "Retiro", "Transferencia", "Pago"]),
+            "monto": monto,
+            "descripcion": descripcion
         }
         
         # Agregar el movimiento a la lista de fechas generadas
@@ -34,36 +46,29 @@ def generarFechas(fecha_desde, fecha_hasta):
     
     return json_generado
 
-#modelo argentino
-# import locale
-# import random
-# from datetime import datetime, timedelta
 
 # def generarFechas(fecha_desde, fecha_hasta):
-    
-#     locale.setlocale(locale.LC_TIME, 'es_AR.UTF-8')
 #     # Convertir las fechas de cadena a objetos datetime
-
-#     fecha_desde_str = datetime.strptime(fecha_desde, "%d/%m/%Y")
-#     fecha_hasta_str = datetime.strptime(fecha_hasta, "%d/%m/%Y")
+#     fecha_desde_dt = datetime.strptime(fecha_desde, "%Y%m%d")
+#     fecha_hasta_dt = datetime.strptime(fecha_hasta, "%Y%m%d")
 #     fechas_generadas = []
     
 #     for _ in range(5):  # Generar cinco fechas
 #         # Generar un número aleatorio de días entre fecha_desde y fecha_hasta
-#         diferencia_dias = (fecha_hasta_str - fecha_desde_str).days
+#         diferencia_dias = (fecha_hasta_dt - fecha_desde_dt).days
 #         dias_aleatorios = random.randint(0, diferencia_dias)
         
 #         # Calcular la fecha aleatoria
-#         fecha_generada = fecha_desde_str + timedelta(days=dias_aleatorios)
+#         fecha_generada = fecha_desde_dt + timedelta(days=dias_aleatorios)
         
-#         # Formatear la fecha como "dd-mm-yyyy"
-#         fecha_formateada = fecha_generada.strftime("%d-%m-%Y")
+#         # Formatear la fecha como "yyyymmdd"
+#         fecha_formateada = fecha_generada.strftime("%Y%m%d")
         
 #         # Crear un diccionario para el movimiento
 #         movimiento = {
 #             "fecha": fecha_formateada,
 #             "tipo": random.choice(["Depósito", "Retiro", "Transferencia", "Pago"]),
-#             "monto": round(random.uniform(-1000, 1000), 2),
+#             "monto": round(random.uniform(-10000, 10000), 2),
 #             "descripcion": random.choice(["Depósito en efectivo", "Retiro de cajero automático", "Transferencia a otra cuenta", "Pago de impuestos y servicios"])
 #         }
         
@@ -74,7 +79,3 @@ def generarFechas(fecha_desde, fecha_hasta):
 #     json_generado = {"movimientos": fechas_generadas}
     
 #     return json_generado
-
-# # fecha_desde_str = "01/01/2023"
-# # fecha_hasta_str = "30/01/2023"
-# # print(generarFechas(fecha_desde_str,fecha_hasta_str))
