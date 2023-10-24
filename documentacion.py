2394ad18a3315cb77fa4abdc39b1675929d40f28
MarianoImende <98611190+MarianoImende@users.noreply.github.com>
2023-10-24 15:00:53 -0300

    Update documentacion.py

--- documentacion.py
+++ documentacion.py
@@ -11,14 +11,13 @@ def documentacion_sesion() -> Dict:
         documentation = {
         "summary": "Crear un nuevo token",
         "description": "Ejemplo de solicitud:",
-        #"response_model": JsonUserRequest,
         "response_description": "Access token creado satisfactoriamente",
         "responses": {
             200: {
                 "description": "Token creado satisfactoriamente",
                 "content": {
                     "application/json": {
-                        "example": {"access_token": "My token", "token_type": "My type"}
+                        "example": {"access_token": "string", "token_type": "string"}
                     }
                 }
             },
@@ -27,6 +26,14 @@ def documentacion_sesion() -> Dict:
                 "content": {
                     "application/json": {
                         "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
                     }
                 }
             }
@@ -41,33 +48,39 @@ class Tarjeta(BaseModel):
 def documentacion_cuentas() -> Dict:
 
         documentation = {
-        "summary": "Obtiene cuentas",
+        "summary": "Obtiene cuentas en base al numero de tarjeta enviado",
         "description": "Ejemplo de solicitud:",
-        #"response_model": JsonUserRequest,
         "response_description": "listado de cuentas asociadas a la tarjeta",
         "responses": {
             200: {
                 "description": "Cuentas obtenida satisfactoriamente",
                 "content": {
                     "application/json": {
-                        #"example": {"access_token": "My token", "token_type": "My type"}
-                        "example": {"cuentas": [{"numero_cuenta": "123456789", "tipo": "CA USD"}]}
+                        "example": {"cuentas": [{"numero_cuenta": "string", "tipo": "string"}]}
                     }
                 }
             },
-            401: {
-                "description": "Error",
+            504: {
+                "description": "Bad request",
                 "content": {
                     "application/json": {
-                        "example": {"detail": "No se ha enviado el numero de tarjeta"}
+                        "example": {"detail": "El campo 'numero_tarjeta' es inválido."}
                     }
                 }
             },
             401: {
-                "description": "Error",
+                "description": "Unauthorized",
                 "content": {
                     "application/json": {
                         "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
                     }
                 }
             }
@@ -83,32 +96,39 @@ class Cuenta(BaseModel):
 def documentacion_saldo() -> Dict:
 
         documentation = {
-        "summary": "Obtiene saldo",
+        "summary": "Obtiene el saldo en base a un numero de cuenta.",
         "description": "Ejemplo de solicitud:",
-        #"response_model": JsonUserRequest,
         "response_description": "Informa el saldo de la cuenta",
         "responses": {
             200: {
-                "description": "saldo obtenido satisfactoriamente",
+                "description": "Saldo obtenido satisfactoriamente",
                 "content": {
                     "application/json": {
-                        "example": {"saldo": "My saldo"}
+                        "example": {"saldo": True}
                     }
                 }
             },
             400: {
-                "description": "Error",
+                "description": "Bad request",
                 "content": {
                     "application/json": {
-                        "example": {"detail": "No se ha enviado el numero_tarjeta de cuenta"}
+                        "example": {"detail": "El campo 'numero' es inválidos"}
                     }
                 }
             },
             401: {
-                "description": "Error",
+                "description": "Unauthorized",
                 "content": {
                     "application/json": {
                         "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
                     }
                 }
             }
@@ -126,7 +146,6 @@ def documentacion_mov() -> Dict:
         documentation = {
         "summary": "Obtiene los movimientos por rango de fechas",
         "description": "Ejemplo de solicitud:",
-        #"response_model": JsonUserRequest,
         "response_description": "Informa los movimientos de la cuenta",
         "responses": {
             200: {
@@ -134,13 +153,13 @@ def documentacion_mov() -> Dict:
                 "content": {
                     "application/json": {
                         
-                        "example": {"movimientos": [{"fecha": "20230131", "monto": "-853.99", "descripcion": "Retiro de cajero automático"}]}
+                        "example": {"movimientos": [{"fecha": "string", "monto": "string", "descripcion": "string"}]}
                        
                     }
                 }
             },
             400: {
-                "description": "Error",
+                "description": "Bad request",
                 "content": {
                     "application/json": {
                         "example": {"detail": "Datos inválidos"}
@@ -148,10 +167,88 @@ def documentacion_mov() -> Dict:
                 }
             },
             401: {
-                "description": "Error",
+                "description": "Unauthorized",
                 "content": {
                     "application/json": {
                         "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
+                    }
+                }
+            }
+        }
+    }
+        return documentation
+    
+def documentacion_estado() -> Dict:
+
+        documentation = {
+        "summary": "Obtiene el estado del usuario",
+        "description": "Ejemplo de solicitud: para este recurso, no de debe enviar un body, simplemente ingrese el token en la interafaz authorizations (botón en la esquina superior derecha de su pantalla en los autodocs de Swagger UI (en /docs) icono del candado), donde puede escriba su clave API en el el campo value. Esto establecerá el Authorization encabezado en los encabezados de la solicitud.",
+        "response_description": "Informa el estado del usuario",
+        "responses": {
+            200: {
+                "description": "información obtenidos satisfactoriamente",
+                "content": {
+                    "application/json": {
+                        "example":{"username": "string", "email": "string", "disabled": "boolean", "hashed_password": "string"}
+                    }
+                }
+            },
+            401: {
+                "description": "Unauthorized",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
+                    }
+                }
+            }
+        }
+    }
+        return documentation
+
+def documentacion_loout() -> Dict:
+
+        documentation = {
+        "summary": "Cierra la sesion",
+        "description": "Ejemplo de solicitud: para este recurso, no de debe enviar un body, simplemente ingrese el token en la interafaz authorizations (botón en la esquina superior derecha de su pantalla en los autodocs de Swagger UI (en /docs) icono del candado), donde puede escriba su clave API en el el campo value. Esto establecerá el Authorization encabezado en los encabezados de la solicitud.",
+        "response_description": "Deshabilita el token",
+        "responses": {
+            200: {
+                "description": "proceso termindao satisfactoriamente",
+                "content": {
+                    "application/json": {
+                        "example": {"message": "Has cerrado sesión exitosamente"}
+                    }
+                }
+            },
+            401: {
+                "description": "Unauthorized",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "No se pudo validar las credenciales"}
+                    }
+                }
+            },
+            500: {
+                "description": "Generic Error",
+                "content": {
+                    "application/json": {
+                        "example": {"detail": "Generic Error"}
                     }
                 }
             }
